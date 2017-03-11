02d710a7e6eb583e25c8cb5e795588077273b0de
Jimmy <jimmyytran@gmail.com>
2017-03-11 20:04:34 +0000

    edits to data reading

--- transfer_learning_on_stack_exchange_tags/train-theano.py
+++ transfer_learning_on_stack_exchange_tags/train-theano.py
@@ -12,6 +12,7 @@ from datetime import datetime
 from utils import *
 from rnn_theano import RNNTheano
 from IPython import embed
+import re
 
 _VOCABULARY_SIZE = int(os.environ.get('VOCABULARY_SIZE', '20000'))
 _HIDDEN_DIM = int(os.environ.get('HIDDEN_DIM', '2000'))
@@ -19,6 +20,7 @@ _LEARNING_RATE = float(os.environ.get('LEARNING_RATE', '0.005'))
 _NEPOCH = int(os.environ.get('NEPOCH', '100'))
 _MODEL_FILE = os.environ.get('MODEL_FILE')
 _EVALUATE_ = os.environ.get('EVALUATE', False)
+_CSV_FILE = os.environ.get('CSV_FILE', 'preprocess/biology_light_punctuation_with_stop.csv')
 
 def train_with_sgd(model, X_train, y_train, learning_rate=0.005, nepoch=1, evaluate_loss_after=5):
     # We keep track of the losses so we can plot them later
@@ -53,71 +55,82 @@ unknown_token = "UNKNOWN_TOKEN"
 sentence_start_token = "SENTENCE_START"
 sentence_end_token = "SENTENCE_END"
 
-# Read the data and append SENTENCE_START and SENTENCE_END tokens
-csv_file = 'preprocess/biology_light_punctuation_with_stop.csv'
-print "Reading CSV file..."
-#  with open('data/reddit-comments-2015-08.csv', 'rb') as f:
-with open(csv_file, 'rb') as f:
-    reader = csv.reader(f, skipinitialspace=True)
-    reader.next()
-    # Split full comments into sentences
-    #  sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode('utf-8').lower()) for x in reader])
-    sentences = itertools.chain(*[nltk.sent_tokenize(x[2].decode('utf-8').lower()) for x in reader])
-    # Append SENTENCE_START and SENTENCE_END
-    sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
-print "Parsed %d sentences." % (len(sentences))
-    
-# Tokenize the sentences into words
-tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
+def load_data_set(csv_file, vocabulary_size):
+	# Read the data and append SENTENCE_START and SENTENCE_END tokens
+	print "Reading CSV file..."
+	#  with open('data/reddit-comments-2015-08.csv', 'rb') as f:
+	with open(csv_file, 'rb') as f:
+	    reader = csv.reader(f, skipinitialspace=True)
+	    reader.next()
+	    # Split full comments into sentences
+	    #  sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode('utf-8').lower()) for x in reader])
+	    sentences = itertools.chain(*[nltk.sent_tokenize(x[2].decode('utf-8').lower()) for x in reader])
+	    # Append SENTENCE_START and SENTENCE_END
+	    sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
+	print "Parsed %d sentences." % (len(sentences))
+	    
+	# Tokenize the sentences into words
+	tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
+	
+	pickle_dir = 'token'
+	#with open(picke_dir + csv_filehhhhhhhhhhhhhhh
 
-# Count the word frequencies
-word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
-print "Found %d unique words tokens." % len(word_freq.items())
+	# Count the word frequencies
+	word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
+	print "Found %d unique words tokens." % len(word_freq.items())
 
-# Get the most common words and build index_to_word and word_to_index vectors
-vocab = word_freq.most_common(vocabulary_size-1)
-index_to_word = [x[0] for x in vocab]
-index_to_word.append(unknown_token)
-word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
+	# Get the most common words and build index_to_word and word_to_index vectors
+	vocab = word_freq.most_common(vocabulary_size-1)
+	index_to_word = [x[0] for x in vocab]
+	index_to_word.append(unknown_token)
+	word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
 
-print "Using vocabulary size %d." % vocabulary_size
-print "The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1])
+	print "Using vocabulary size %d." % vocabulary_size
+	print "The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1])
 
-# Replace all words not in our vocabulary with the unknown token
-for i, sent in enumerate(tokenized_sentences):
-    tokenized_sentences[i] = [w if w in word_to_index else unknown_token for w in sent]
-
-# Create the training data
-X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized_sentences])
-y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
-
-
-model = RNNTheano(vocabulary_size, hidden_dim=_HIDDEN_DIM)
-t1 = time.time()
-model.sgd_step(X_train[10], y_train[10], _LEARNING_RATE)
-t2 = time.time()
-print "SGD Step time: %f milliseconds" % ((t2 - t1) * 1000.)
-if _MODEL_FILE != None:
-    load_model_parameters_theano(_MODEL_FILE, model)
+	# Replace all words not in our vocabulary with the unknown token
+	for i, sent in enumerate(tokenized_sentences):
+	    tokenized_sentences[i] = [w if w in word_to_index else unknown_token for w in sent]
+	return tokenized_sentences, index_to_word, word_to_index
 
 if not _EVALUATE_:
+    model = RNNTheano(vocabulary_size, hidden_dim=_HIDDEN_DIM)
+
+    if _MODEL_FILE != None:
+	m = re.findall('\d+',model_location)
+	vocabulary_size = m[1]
+	model = RNNTheano(vocabulary_size, hidden_dim=_HIDDEN_DIM)
+        load_model_parameters_theano(_MODEL_FILE, model)
+
+    tokenized_sentences, index_to_word, word_to_index = load_data_set(csv_file, vocabulary_size)
+
+    # Create the training data
+    X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized_sentences])
+    y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
+    
     print "Begin training"
     train_with_sgd(model, X_train, y_train, nepoch=_NEPOCH, learning_rate=_LEARNING_RATE)
 else:
-    print "Start IPython"
+    print "Start IPython for Evaluation"
+
     def generate_sentence(model):
         # We start the sentence with the start token
         new_sentence = [word_to_index[sentence_start_token]]
         # Repeat until we get an end token
+    	unknown_count = 0
         while not new_sentence[-1] == word_to_index[sentence_end_token]:
             next_word_probs = model.forward_propagation(new_sentence)
             sampled_word = word_to_index[unknown_token]
             # We don't want to sample unknown words
             while sampled_word == word_to_index[unknown_token]:
+		sys.out.write('.')
                 samples = np.random.multinomial(1, next_word_probs[-1])
                 sampled_word = np.argmax(samples)
+	    #sys.stdout.write(str(index_to_word[sampled_word]) + ": "+ str(next_word_probs[-1][sampled_word]) + " ")
             new_sentence.append(sampled_word)
+    	#sys.stdout.write("uk= " + str(unknown_count) + " ")
         sentence_str = [index_to_word[x] for x in new_sentence[1:-1]]
+	#sys.stdout.write('|')
         return sentence_str
 
     def gen(num_sentences, senten_min_length):
@@ -126,6 +139,23 @@ else:
             # We want long sentences, not sentences with one or two words
             while len(sent) < senten_min_length:
                 sent = generate_sentence(model)
+	    print ""
             print " ".join(sent)
+
+    def import_model(model_location, csv_file=_CSV_FILE):
+	m = re.findall('\d+',model_location)
+	vocab_size = int(m[1])
+	hidden_d = int(m[0])
+	print "Vocab size= %d, hidden dimensions= %d" % (vocab_size, hidden_d)
+	model2 = RNNTheano(vocab_size, hidden_dim=hidden_d)
+	load_model_parameters_theano(model_location, model2)
+	_, idx_to_word, word_to_idx = load_data_set(csv_file, vocab_size)
+	return model2, idx_to_word, word_to_idx
+	#model = model2
+	#word_to_index = word_to_idx
+	#idx_to_word = index_to_word
+
+    if _MODEL_FILE != None:
+	model, index_to_word, word_to_index = import_model(_MODEL_FILE)
 
     embed()
